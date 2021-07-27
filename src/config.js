4fc60288c35c38c94a7c41276cbb37da89ec27f1
Olga <ohalytska@griddynamics.com>
2021-07-27 12:23:51 +0300

    TimeChimp React Coding Challenge

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ const config = {
     baseUrl: "https://api.spotify.com/v1",
     authUrl: "https://accounts.spotify.com/api/token",
     clientId: "37065b25c0c74083b4b78e20bd1d250f",
-    clientSecret: "d1e55ae1018943479a969129fe3f19dd",
     redirectUrl: "http://localhost:3000/",
     scope: "user-read-private user-read-email user-top-read playlist-modify-private",
     url: "https://accounts.spotify.com/authorize",
